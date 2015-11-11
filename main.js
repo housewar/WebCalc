e159d2f29ab996b8ee86db31c865f9cf0f94b062
mchouse <general.kim.jong.il@gmail.com>
2015-11-11 16:54:29 -0600

    operations
    added functionality for basic operations

--- main.js
+++ main.js
@@ -2,21 +2,67 @@ $(document).ready(function(){
   
   function Calculator(){
     var numArr = [];
-    this.currentNum;
+    var op;
+    this.currentNum = 0;
+    this.subTotal = 0;
+    this.total = 0;
+    var fin = false;
     
     Calculator.prototype.addDigit = function(num){
     	numArr.push(num);
     	numArr = Number(numArr.join("")).toString().split("").slice(0,10); //chops off leading zeroes and limits number of digits.
     	this.currentNum = Number(numArr.join(""));
     }
-    Calculator.prototype.addDecimal = function(num){
+    Calculator.prototype.addDecimal = function(){
     	if (numArr.indexOf(".") === -1){
     		numArr.push(".");
     	}
     }
-    Calculator.prototype.clearEntry = function(num){
+    Calculator.prototype.clearEntry = function(){
     	numArr = [];
     	this.currentNum = 0;
+    }
+    Calculator.prototype.setOp = function(operation){
+    	op = operation;
+    	this.subTotal = this.currentNum;
+    	numArr = [];
+    }
+    Calculator.prototype.result = function(fin){
+    	
+    	switch (op) {
+    		case '+':
+    		  this.sum();
+    		  break;
+    		case '-':
+    		  this.diff();
+    		  break;
+    		case '/':
+    		  this.quotient();
+    		  break;
+    		case '*':
+    		  this.product();
+    		  break;
+    	}
+    	
+    	if (fin = true) {
+    		this.total = this.subTotal;
+    		this.currentNum = this.subTotal;
+    		this.subTotal = 0;
+    		numArr = [];
+    	}
+    	
+    }
+    Calculator.prototype.sum = function(){
+      this.subTotal += this.currentNum;
+    }
+    Calculator.prototype.diff = function(){
+      this.subTotal -= this.currentNum;
+    }
+    Calculator.prototype.quotient = function(){
+      this.subTotal /= this.currentNum;
+    }
+    Calculator.prototype.product = function(){
+      this.subTotal *= this.currentNum;
     }
   }
   
@@ -37,13 +83,22 @@ $(document).ready(function(){
           if (Math.floor(myCalc.currentNum) === myCalc.currentNum){
             $(".display").text(myCalc.currentNum + ".");
           }
-      break;
+        break;
       case "CE":
       	myCalc.clearEntry();
       	$(".display").text(0);
-      break;
+        break;
+      case "+":
+      case "-":
+      case "/":
+      case "*":
+      	myCalc.setOp(func);
+      	$(".operation").text(func);
+        break;
+      case "=":
+        myCalc.result(true);
+        $(".display").text(myCalc.total);
+        break;
     }
-    
-    
   });
 });
